c595f885671529e2c20cbfd722b4661bbdb8f39e
maijaelinalaine <maijaelina05@gmail.com>
2026-03-16 14:04:40 +0200

    complete exercises 1.1-5

---
+++ part1/part1.1/src/components/Total.jsx
@@ -0,0 +1,5 @@
+export default function Total({ parts }) {
+  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
+
+  return <p>Number of exercises {totalExercises}</p>;
+}
