6c779b7b54e7811aa32b08894aa348b86da4902b
maijaelinalaine <maijaelina05@gmail.com>
2026-03-18 14:06:24 +0200

    complete part 1

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+const Statistics = ({ good, neutral, bad }) => {
+  if (good + neutral + bad === 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>no feedback given</p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1>statistics</h1>
+      <tr>
+        <td>good</td>
+        <td>{good}</td>
+      </tr>
+      <tr>
+        <td>neutral</td>
+        <td>{neutral}</td>
+      </tr>
+      <tr>
+        <td>bad</td>
+        <td>{bad}</td>
+      </tr>
+      <tr>
+        <td>all</td>
+        <td>{good + neutral + bad}</td>
+      </tr>
+      <p>
+        average {(good * 1 + neutral * 0 + bad * -1) / (good + neutral + bad)}
+      </p>
+      <p>positive {(good / (good + neutral + bad)) * 100}%</p>
+    </div>
+  );
+};
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
+};
+
+export default App;
